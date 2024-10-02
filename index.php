f87382c8268bdff64640edaa93d1332fa7d6dd22
Jashan Pal Singh <jsing785@mtroyal.ca>
2024-10-02 13:43:15 -0600

    "added markup to index, created css, added config.inc.php"

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include "includes/config.inc.php";
+
+?>
+
+<html lang="en">
+    <head>
+        <title>F1 Dashboard</title>
+        <link rel="stylesheet" type="text/css" href= "css/generalstyle.css">
+        <meta charset="utf-8">
+        <meta name="description" content="Assignment #1 for COMP3512 at Mount Royal University">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!--SOURCE:  https://fonts.google.com/selection?classification=Display&stroke=Sans+Serif-->
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <?php createHeader() ?>
+        <main id="main">
+            <article>
+                <h2><b>Description</b></h2>
+                <p>
+                    This project was created by Ishan Ishan and Jashan Pal Singh of COMP3512 at the Mount Royal University taught by Professor Randy Conolly.
+                </p>
+            </article>
+            <article>
+                display pics here
+            </article>
+        </main>
+    </body>
+</html>
