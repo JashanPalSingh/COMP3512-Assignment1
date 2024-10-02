7e2fb0559a0e3e85c0117c64320dd00030668675
Jashan Pal Singh <jsing785@mtroyal.ca>
2024-10-02 14:15:11 -0600

    Added content to index.php, View repo button needs fixing

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include "includes/config.inc.php";
         <meta name="description" content="Assignment #1 for COMP3512 at Mount Royal University">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!--SOURCE:  https://fonts.google.com/selection?classification=Display&stroke=Sans+Serif-->
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
     </head>
     <body>
@@ -19,8 +20,15 @@ include "includes/config.inc.php";
             <article>
                 <h2><b>Description</b></h2>
                 <p>
-                    This project was created by Ishan Ishan and Jashan Pal Singh of COMP3512 at the Mount Royal University taught by Professor Randy Conolly.
+                    This site provides you with the lastest information about the 2022 F1 season. You can browse the season's race results, constructor details, as well as the driver statistics. 
                 </p>
+                <p>    
+                    This project was created by Ishan Ishan and Jashan Pal Singh as their Assignment 1 studying COMP3512 at the Mount Royal University taught by Professor Randy Conolly.
+                </p>
+                <p>
+                    You can access our GitHub repository for this project below
+                </p>
+                <a href="https://github.com/JashanPalSingh/COMP3512-Assignment1" id="viewrepo"> VIEW REPO</a>
             </article>
             <article>
                 display pics here
