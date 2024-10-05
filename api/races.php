1e9c6ec95236ebcaf658da7561abde0742dee3fc
Jashan Pal Singh <jsing785@mtroyal.ca>
2024-10-05 14:57:02 -0600

    improved races.php

--- api/races.php
+++ api/races.php
@@ -5,10 +5,17 @@ header('content-type: application/json');
 header("Access-Control-Allow-Origin: *");
 try{
     $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
-    
+    if (! isset ($_GET['raceId'])){
     $SQL = 'SELECT `round`, `name`, raceId FROM races WHERE races.`year` = 2022 ORDER BY `round`;';
     $statement = $pdo -> prepare($SQL);
     $statement -> execute();
+    }
+    else{
+        $SQL = 'SELECT races.`name`,`round`, circuits.`name`, circuits.`location`, circuits.`country`, races.`date`, races.url, raceId FROM races JOIN circuits ON races.circuitId = circuits.circuitId WHERE races.raceId=?;';
+        $statement = $pdo -> prepare($SQL);
+        $statement -> bindValue(1, $_GET['raceId']);
+        $statement -> execute();
+    }
     echo json_encode($statement -> fetchAll(PDO::FETCH_ASSOC));
     $pdo = null;
 }catch (PDOException $e){
