d40071500b247e6861c6f8c316161986be2f7b6c
Jashan Pal Singh <jsing785@mtroyal.ca>
2024-10-07 14:15:08 -0600

    Modified browse.php to incluse list, created phpconfig.inc.php file

---
+++ includes/phpconfig.inc.php
@@ -0,0 +1,8 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'f1');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBCONNSTRING','sqlite:./data/f1.db');
+// define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+?>
