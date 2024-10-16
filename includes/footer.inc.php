5700360d99c9754806f0733a4bc1d2318f060194
Ishan <iisha595@mtroyal.ca>
2024-10-16 03:26:43 -0600

    changed logo href to github repo

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,7 +1,7 @@
 <?php
 function createFooter(){
 echo "<footer>";
-echo "<a href='index.php'><img src='css/ij.png' alt='An Ishan Jashan Project'></a>";
+echo "<a href='https://github.com/JashanPalSingh/COMP3512-Assignment1' id='viewrepo' target='_blank'><img src='css/ij.png' alt='An Ishan Jashan Project'></a>";
     
 echo "<p>Not officialy endorsed by the F1 or any of it's subsidiaries.<br> Ishan and Jashan &copy 2024.</p>";
 echo "</footer>";
